4186efc4d674cd23b922345a89e2837584151474
Mykhailo <babkin.michail.2005@gmail.com>
2025-11-07 14:13:53 -0500

    Done

--- Home.py
+++ Home.py
@@ -7,13 +7,13 @@ gemini_request = GeminiRequest("gemini-2.5-flash-image")
 def run(): 
     image_path = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-    with open(image_path.name, "wb") as f:
-        f.write(image_path.getbuffer())
-
     prompt = "Add beer to this image. Either a beer bottle or a beer glass where it is most suitable"
 
     if st.button("Add beer") and image_path is not None:
         with st.spinner("Adding beer..."):
+
+            with open(image_path.name, "wb") as f:
+                f.write(image_path.getbuffer())
             gemini_request.edit_images(prompt, image_path.name)
 
             # Display the generated image
